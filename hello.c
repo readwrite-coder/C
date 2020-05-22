8c18504bc0f34dfdd53290ef7c82eaf96bf04ae5
Andrew Trinh <andrewtrinh@Andrews-MacBook-Air.local>
2020-05-22 14:58:38 -0700

    0.25 C learning

---
+++ hello.c
@@ -0,0 +1,28 @@
+//creating my first C program using Vim text editor.
+//cursor is just a text box that hovers on the display.
+//algorithm: step by step instructions.
+//anything that a computer can do comes down to a modular function.
+//learning curve for programming requires knowledge 
+//not easily digestable at once. 
+//functions give us a toolbox of commands we can tell the computer.
+//programs can call other programs like a chain; returning '0' indicates
+//everything went ok. Alyways want to start program with main, the starting
+//off point for your computer function.
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World!\n");
+	return 0;
+}
+
+//gcc compiles our source code into machine language 
+//that gets processed by the computer processor.
+//to insert in VIM click 'i', then 'esc' to exit.
+//to save changes in VIM 'esc', then ':w'.
+//to quit VIM click ':q'.
+
+
+
+
