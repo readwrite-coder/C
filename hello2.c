03d4dc05dd47eb1ef6811ff739fea7e510e01900
Andrew Trinh <andrewtrinh@Andrews-MacBook-Air.local>
2020-05-22 17:30:19 -0700

    1.03 basic output to console

---
+++ hello2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	x = 5;
+	//syntax = form format of the language
+	
+	int y = x / 2;
+	//Expression - anything that can be evaluated - result: 1 value
+	//statement
+	//declaration
+	//initialization	
+	
+	//can't just print integers, must type in extra stuff to format console output.
+	//format string: conversion characters convert number to form console allows.
+	printf("The magic number is: %i\nThe value of y is %i!\n",9001,y);
+	//everytime we have something in between parentheses seperated by comma, we have 
+	//something called arguments.
+	return 0;
+}
+
+//compiler errors tell us to make sure our program is correct before it can be compiled.
+//
